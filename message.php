7ac0c079fcd9213562b3d662e3000a3b92f251d7
dovlie <541728846@qq.com>
2016-11-25 22:06:31 +0800

    Update message.php

--- message.php
+++ message.php
@@ -19,16 +19,7 @@ if(mysql_query("INSERT INTO metexts VALUES ('$uname', '$Metext','$time')")){
 	// echo "111";
 }
 else{
-	echo "留言失败！再试一遍呗<META HTTP-EQUIV=\"refresh\" CONTENT=\"5;url='T-main.html'\">";
+	echo "留言失败！再试一遍<META HTTP-EQUIV=\"refresh\" CONTENT=\"5;url='T-main.html'\">";
 }
-
-// if($row = mysql_fetch_array($result))
-//   {
-//   // echo $row['FirstName'] . " " . $row['LastName'];
-//   // echo "<br />";
-//   	echo "登陆成功！<META HTTP-EQUIV=\"refresh\" CONTENT=\"0;url='Tmainpage.html?uname=$uname'\">"; 
-//   }else{
-//   	echo "登录失败，该用户不存在...请重新登录<META HTTP-EQUIV=\"refresh\" CONTENT=\"2;url='T-main.html'\">";
-//   }
 
 ?>
